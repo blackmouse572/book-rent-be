87adb05099b0257830640357390d8de3ee107390
SaNgoTin <phuoctungpbc02@gmail.com>
2023-10-26 21:31:45 +0700

    feat: add cloudinary key to env

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -4,9 +4,9 @@ export const CloudinaryProvider = {
     provide: 'CLOUDINARY',
     useFactory: () => {
         return cloudinary.config({
-            cloud_name: 'dvzcpzqiw',
-            api_key: '913793345811299',
-            api_secret: 'Z9tKEPFtIp57Y2YtkPtMrhso718',
+            cloud_name: process.env.CLOUDINARY_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET,
         });
     },
 };
